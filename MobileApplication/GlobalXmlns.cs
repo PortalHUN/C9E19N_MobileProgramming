e27d66793a3ed1179d13eaa0f9eff930d3a6aa5b
PortalHUN <giotomi03@gmail.com>
2025-11-20 13:15:58 +0100

    Initialized both projects

---
+++ MobileApplication/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MobileApplication")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MobileApplication.Pages")]
