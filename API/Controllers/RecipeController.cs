446aaeff7eb459ac7ab3fdf5e21657982ee4a093
PortalHUN <giotomi03@gmail.com>
2025-11-20 13:36:19 +0100

    Created API Model and Controller

---
+++ API/Controllers/RecipeController.cs
@@ -0,0 +1,18 @@
+﻿using API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class RecipeController : Controller
+  {
+    static List<Recipe> recipes = new List<Recipe>();
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      return Ok(recipes);
+    }
+  }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f811374077089fcc023cea4d72cb5a038f9362d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
