f3247a786582a645a3b56312e56c6c47c8c72d53
PortalHUN <giotomi03@gmail.com>
2025-11-23 17:28:10 +0100

    RecipeDetail xml done

--- MobileApplication/Pages/RecipeDetail/RecipeDetail.xaml.cs
+++ MobileApplication/Pages/RecipeDetail/RecipeDetail.xaml.cs
@@ -7,5 +7,11 @@ public partial class RecipeDetail : ContentPage
 	{
 		InitializeComponent();
 		this.vm = vm;
+		BindingContext = vm;
 	}
+
+  protected override void OnNavigatedTo(NavigatedToEventArgs args)
+  {
+    base.OnNavigatedTo(args);
+  }
 }
